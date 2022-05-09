de6ec02718e9a06964ac9c62b7c6a51df0d7c8f6
Chloe(노기진) <gjnoh@gabia.com>
2022-05-09 14:00:34 +0900

    style: 댓글 페이지 마크업 - multi-word lint 규칙 수정

---
+++ src/components/Introduction.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="introduction">
+    <img src="@/assets/images/gijin.png" alt="아바타 이미지" />
+    <div class="name">{{ name }}</div>
+    <div class="description">{{ description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '위니',
+      description: '안녕하세요 프론트엔드개발팀 신입사원 위니입니다.',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.introduction {
+  img {
+    display: block;
+    margin: 0px auto;
+  }
+  .name {
+    margin-bottom: 7px;
+  }
+
+  .description {
+    display: flex;
+    align-items: center;
+    width: 327px;
+    height: 62px;
+    padding: 12px 31px;
+    background: #ecf7ff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 0px 26px 26px 26px;
+  }
+}
+</style>
--- src/pages/CommentPage.vue
+++ src/pages/CommentPage.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>코멘트</div>
+  <main class="comment-wrapper">
+    <div class="comment">
+      <Introduction />
+    </div>
+  </main>
 </template>
 
 <script>
-export default {};
+import Introduction from '@/components/Introduction.vue';
+
+export default {
+  components: {
+    Introduction,
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.comment-wrapper {
+  height: 100vh;
+  .comment {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    width: 769px;
+    height: 100%;
+    margin: 0 auto;
+    padding: 43px 48px 38px 48px;
+    background: #f2f3f5;
+  }
+}
+</style>
