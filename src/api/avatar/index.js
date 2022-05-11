de16ebc96586d238379d128b94e0e87f3870c29c
Chloe(노기진) <gjnoh@gabia.com>
2022-05-11 17:40:38 +0900

    chore: 사용안하는 파일 제거

--- src/api/avatar/index.js
+++ src/api/avatar/index.js
@@ -1,7 +1,6 @@
 import gaxios from '@/api/config/axios';
 
 const avatarApi = {
-  getAvatar: ({ id }) => gaxios.get(`/avatars/${id}`),
   getAvatars: () => gaxios.get('/avatars'),
   getCertainUser: ({ id }) => gaxios.get(`/avatars/${id}?populate=comments`),
 };
