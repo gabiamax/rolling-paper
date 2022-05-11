74f338f069ef40bfa058ee44fc86c67061f1ffb9
Lime(임소형) <shyim@gabia.com>
2022-05-11 15:43:34 +0900

    fix: 캐릭터 사이즈 반응형으로 수정

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -27,10 +27,12 @@ export default {
     // });
     // TODO 임시: 뿌려줄 때 마다 랜덤 값 지정
     data.forEach((avatar) => {
-      const { x, y } = this.getRandomXY(SIZE.WRAPPER, SIZE.AVATAR);
+      // const { x, y } = this.getRandomXY(SIZE.WRAPPER, SIZE.AVATAR);
+      const unit = '%';
+      const { x, y } = this.getRandomXY(SIZE.WRAPPER, SIZE.AVATAR, unit);
 
       /* eslint-disable */
-      avatar.style = { top: y+'px', left: x+'px' };
+      avatar.style = { top: y, left: x};
     });
     this.avatars = data;
   },
@@ -42,9 +44,13 @@ export default {
     getRandomNumber(min, max) {
       return Math.floor(Math.random() * (max - min) + min);
     },
-    getRandomXY(wrapper, avatar) {
-      const x = this.getRandomNumber(0, wrapper.width - avatar.width);
-      const y = this.getRandomNumber(0, wrapper.height - avatar.height);
+    getRandomXY(wrapper, avatar, unit='px') {
+      const maxX = wrapper.width - avatar.width;
+      const maxY = wrapper.height - avatar.height;
+
+      const x = this.getRandomNumber(0, maxX) + unit;
+      const y = this.getRandomNumber(0, maxY) + unit;
+      console.log({x, y});
       return { x, y };
     },
   },
